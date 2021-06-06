41bc18592ded7994acacb47f5d23d5b962173bf2
Rutvi Sukhadia <rutmahee2015@gmail.com>
2021-06-06 22:27:28 +0530

    cardList component added

--- robo-friends/src/index.js
+++ robo-friends/src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import {robots} from './robots'
-import Card from './Card';  //by default considers card.js if type not sprcified
+import CardList from './CardList';  //by default considers card.js if type not sprcified
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email}/>
-    
-    </div>
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
