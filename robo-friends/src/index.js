0e69190aee199c8e1a3b70a17786dfde2ff1888b
Rutvi Sukhadia <rutmahee2015@gmail.com>
2021-06-08 01:47:07 +0530

    added redux-logger

--- robo-friends/src/index.js
+++ robo-friends/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';  
+import {createStore,applyMiddleware} from 'redux'; 
+import {createLogger} from 'redux-logger'; 
 import './index.css';
 import 'tachyons';
 import App from './containers/App';  //by default considers app.js if type not sprcified
 import {searchRobots} from './reducers';
 import reportWebVitals from './reportWebVitals';
 
-
-const store = createStore(searchRobots);  //store created that contains state of the app
+const logger = createLogger();
+const store = createStore(searchRobots,applyMiddleware(logger));  //store created that contains state of the app
 
 ReactDOM.render(
   //Provider component passes down the store to all the components down the component tree from the app
